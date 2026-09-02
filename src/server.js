58bb3ef03478fe365604dde43f10a551434071ec
PedroD-code <pdionisio2907@gmail.com>
2026-09-02 17:24:22 -0300

    subindo codigo

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const routes = require('./Routes/routes');
+const sequelize = require('./');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`);
+});
